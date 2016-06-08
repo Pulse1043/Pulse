e5415a7c0cd68faee40f44b64b7594774865c593
Liam Thomas <liam.thomas@ad.aie.edu>
2016-06-08 18:33:06 +0930

    Platformer game

---
+++ AwesomePlatformer/AwesomePlatformer/Sprite.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+// if you called your project something different, make sure you change
+// the namespace
+namespace AwesomePlatformer
+{
+    class Sprite
+    {
+        public Vector2 position = Vector2.Zero;
+        public Vector2 offset = Vector2.Zero;
+        Texture2D texture;
+        public Sprite()
+        {
+        }
+        public void Load(ContentManager content, string asset)
+        {
+            texture = content.Load<Texture2D>(asset);
+        }
+        public void Update(float deltaTime)
+        {
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position + offset, Color.White);
+        }
+    }
+}
